5f8dc7cd2d72260476bf2cedc0863d0a3a961518
marcout <virginie.marcout@emerson.com>
2021-06-29 11:57:40 +0200

    header

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./components/shop/shop.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.comonent";
 
 function App() {
   return (
     <div>
-      {/* <HomePage /> */}
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
