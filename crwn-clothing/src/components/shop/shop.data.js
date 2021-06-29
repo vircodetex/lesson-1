03aaf76feb967e98361f1f0f6caa95d336cdc5e3
marcout <virginie.marcout@emerson.com>
2021-06-29 11:36:41 +0200

    collection item

---
+++ crwn-clothing/src/components/shop/shop.data.js
@@ -0,0 +1,284 @@
+const SHOP_DATA = [
+  {
+    id: 1,
+    title: "Hats",
+    routeName: "hats",
+    items: [
+      {
+        id: 1,
+        name: "Brown Brim",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/c89/hat-1422640.jpg",
+        price: 25,
+      },
+      {
+        id: 2,
+        name: "Blue Beanie",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/c89/hat-1422640.jpg",
+        price: 18,
+      },
+      {
+        id: 3,
+        name: "Brown Cowboy",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/c89/hat-1422640.jpg",
+        price: 35,
+      },
+      {
+        id: 4,
+        name: "Grey Brim",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/c89/hat-1422640.jpg",
+        price: 25,
+      },
+      {
+        id: 5,
+        name: "Green Beanie",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/c89/hat-1422640.jpg",
+        price: 18,
+      },
+      {
+        id: 6,
+        name: "Palm Tree Cap",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/c89/hat-1422640.jpg",
+        price: 14,
+      },
+      {
+        id: 7,
+        name: "Red Beanie",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/c89/hat-1422640.jpg",
+        price: 18,
+      },
+      {
+        id: 8,
+        name: "Wolf Cap",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/c89/hat-1422640.jpg",
+        price: 14,
+      },
+      {
+        id: 9,
+        name: "Blue Snapback",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/c89/hat-1422640.jpg",
+        price: 16,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Sneakers",
+    routeName: "sneakers",
+    items: [
+      {
+        id: 1,
+        name: "Adidas NMD",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/f23/blue-suede-shoes-1-1478929.jpg",
+        price: 220,
+      },
+      {
+        id: 2,
+        name: "Adidas Yeezy",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/f23/blue-suede-shoes-1-1478929.jpg",
+        price: 280,
+      },
+      {
+        id: 3,
+        name: "Black Converse",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/f23/blue-suede-shoes-1-1478929.jpg",
+        price: 110,
+      },
+      {
+        id: 4,
+        name: "Nike White AirForce",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/f23/blue-suede-shoes-1-1478929.jpg",
+        price: 160,
+      },
+      {
+        id: 5,
+        name: "Nike Red High Tops",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/f23/blue-suede-shoes-1-1478929.jpg",
+        price: 160,
+      },
+      {
+        id: 6,
+        name: "Nike Brown High Tops",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/f23/blue-suede-shoes-1-1478929.jpg",
+        price: 160,
+      },
+      {
+        id: 7,
+        name: "Air Jordan Limited",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/f23/blue-suede-shoes-1-1478929.jpg",
+        price: 190,
+      },
+      {
+        id: 8,
+        name: "Timberlands",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/f23/blue-suede-shoes-1-1478929.jpg",
+        price: 200,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Jackets",
+    routeName: "jackets",
+    items: [
+      {
+        id: 1,
+        name: "Black Jean Shearling",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/193/jacket-1476507.jpg",
+        price: 125,
+      },
+      {
+        id: 2,
+        name: "Blue Jean Jacket",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/193/jacket-1476507.jpg",
+        price: 90,
+      },
+      {
+        id: 3,
+        name: "Grey Jean Jacket",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/193/jacket-1476507.jpg",
+        price: 90,
+      },
+      {
+        id: 4,
+        name: "Brown Shearling",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/193/jacket-1476507.jpg",
+        price: 165,
+      },
+      {
+        id: 5,
+        name: "Tan Trench",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/193/jacket-1476507.jpg",
+        price: 185,
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Womens",
+    routeName: "womens",
+    items: [
+      {
+        id: 1,
+        name: "Blue Tanktop",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/f4a/dancing-women-1431085.jpg",
+        price: 25,
+      },
+      {
+        id: 2,
+        name: "Floral Blouse",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/f4a/dancing-women-1431085.jpg",
+        price: 20,
+      },
+      {
+        id: 3,
+        name: "Floral Dress",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/f4a/dancing-women-1431085.jpg",
+        price: 80,
+      },
+      {
+        id: 4,
+        name: "Red Dots Dress",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/f4a/dancing-women-1431085.jpg",
+        price: 80,
+      },
+      {
+        id: 5,
+        name: "Striped Sweater",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/f4a/dancing-women-1431085.jpg",
+        price: 45,
+      },
+      {
+        id: 6,
+        name: "Yellow Track Suit",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/f4a/dancing-women-1431085.jpg",
+        price: 135,
+      },
+      {
+        id: 7,
+        name: "White Blouse",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/f4a/dancing-women-1431085.jpg",
+        price: 20,
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Mens",
+    routeName: "mens",
+    items: [
+      {
+        id: 1,
+        name: "Camo Down Vest",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/74a/man-silhouette-1525996.jpg",
+        price: 325,
+      },
+      {
+        id: 2,
+        name: "Floral T-shirt",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/74a/man-silhouette-1525996.jpg",
+        price: 20,
+      },
+      {
+        id: 3,
+        name: "Black & White Longsleeve",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/74a/man-silhouette-1525996.jpg",
+        price: 25,
+      },
+      {
+        id: 4,
+        name: "Pink T-shirt",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/74a/man-silhouette-1525996.jpg",
+        price: 25,
+      },
+      {
+        id: 5,
+        name: "Jean Long Sleeve",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/74a/man-silhouette-1525996.jpg",
+        price: 40,
+      },
+      {
+        id: 6,
+        name: "Burgundy T-shirt",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/74a/man-silhouette-1525996.jpg",
+        price: 25,
+      },
+    ],
+  },
+];
+
+export default SHOP_DATA;
